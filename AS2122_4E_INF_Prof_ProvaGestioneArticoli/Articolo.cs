593390d0cfed5a6c99e381f49d7d85c4ee565155
Youssra Mabrouki <2703mabrouki@gmail.com>
2022-03-16 09:46:04 +0100

    Problemi con l'avvio del file, (8) aggiungere attributi privati di Articolo - (10) impostazione costruttore classe Articolo come utilizzato da frmMain

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -10,6 +10,10 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
     {
         // TODO: (8) aggiungere attributi privati di Articolo
         // ...
+        int codice;
+        string descrizione;
+        string unitaMisura;
+        double prezzo;
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
@@ -17,6 +21,9 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
             // ...
+            this.descrizione = descrizione;
+            this.unitaMisura = unitaMisura;
+            this.prezzo = prezzo;
         }
 
         public string Visualizzati()
