b846cda03fefcf206d9793301a27507fc97b1314
Youssra Mabrouki <2703mabrouki@gmail.com>
2022-03-16 10:24:25 +0100

    (2) aggiungere visualizzazione articoli inseriti nella listbox

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -51,6 +51,11 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 case "Visualizza articoli":
                     // TODO: (2) aggiungere visualizzazione articoli inseriti nella listbox
                     // ... lstElenco.Items.Add(a.Visualizzati()); 
+    
+                    foreach (Articolo a in articoli)
+                    {
+                        lstElenco.Items.Add(a.Visualizzati());
+                    }
                     break;
             }
         }
