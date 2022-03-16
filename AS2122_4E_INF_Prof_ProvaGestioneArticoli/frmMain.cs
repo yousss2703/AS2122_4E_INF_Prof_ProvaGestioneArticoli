1864cc1cec193d566817dd4895163909ddc74335
Youssra Mabrouki <2703mabrouki@gmail.com>
2022-03-16 09:52:06 +0100

    (1) aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -33,6 +33,11 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 // aggiungi l'articolo all'elenco di articoli
                 // TODO: (1) aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli
                 // ...
+
+                Articolo a = new Articolo();
+                articoli.Add();
+                Articolo a = new Articolo();
+                articoli.Add(a);
                 
                 lblArticoliInseriti.Text = $"Articoli ({articoli.Count})";
             }
