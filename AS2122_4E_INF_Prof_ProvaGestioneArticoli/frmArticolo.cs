43a7b5eb7d62260c939353f57875c55d30e7f456
Youssra Mabrouki <2703mabrouki@gmail.com>
2022-03-16 10:12:22 +0100

    (5) aggiungere attributi privati dei dati inseriti nella frmArticoli

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -17,6 +17,10 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
         // ...
 
+        string descrizione;
+        string unitaMisura;
+        double prezzo;
+
         public DialogResult Status { get { return status; } }
 
         // TODO: (6) aggiungere property di sola lettura dei dati inseriti nella frmArticoli per l'utilizzo in frmMain
